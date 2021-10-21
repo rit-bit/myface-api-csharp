281d57bccbde3e1f52b2e94de3b450f60c9261d4
Richard Sutch <richard.sutch@softwire.com>
2021-10-21 14:27:06 +0100

    Added posts to users page, plus CSS

---
+++ MyFace-react/my-face-react/src/views/Test.data.js
@@ -0,0 +1,146 @@
+const users = [
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    }, {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah. This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    }, {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis Alexis",
+        posts: [
+            {
+                message: "This is my first post. blah blah blah. This is my first post. blah blah blah.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+    {
+        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
+        name: "charles Alexis",
+        posts: [
+            {
+                message: "This is a post.",
+
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            },
+            {
+                message: "This is my first post. blah blah blah."
+            }]
+    },
+]
+
+export default users;
--- MyFace-react/my-face-react/src/views/User.view.js
+++ MyFace-react/my-face-react/src/views/User.view.js
@@ -1,58 +1,41 @@
 import React from 'react';
+import users from './Test.data'
 
-const users = [
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis"
-    },
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis Alexis"
-    },
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis"
-    },{
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles"
-    },{
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis"
-    },
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis"
-    },
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis Alexis"
-    },
-    {
-        image: 'https://media.istockphoto.com/photos/portrait-of-a-smiling-man-of-indian-ethnicity-picture-id1277971635?b=1&k=20&m=1277971635&s=170667a&w=0&h=L1pgfiauaN2wIOgxdetel63XLq2ax3EkbfaIUT67USc=',
-        name: "charles Alexis"
-    },
-
-
-
-]
-
-export function UserView(){
+export function UserView() {
     // user = GEtAPI()
-    return(
+    return (
         <div className={"line-wrap-container"}>
             {users.map((user, index) =>
-                <User key={"user: " + index} image={user.image} name={user.name}/>
+                <UserContainer key={"user: " + index} userdata={user} />
             )}
         </div>
     )
 }
 
-function User(props){
+function UserContainer(props) {
+    return (
+        <div className={"user-profile align-centre"}>
+            <UserProfile image={props.userdata.image} name={props.userdata.name} />
+            {props.userdata.posts.map((post, index) => 
+            <UserRecentPosts post={post}/>)}
+        </div>
+    )
+}
+
+function UserProfile(props) {
+    return (
+        <div className="align-centre">
+            <img src={props.image}
+                className="card profile-image round-image" />
+            <div className="profile-name">{props.name}</div>
+        </div>
+    )
+}
+
+function UserRecentPosts(props) {
     return(
-        <div className={"user-profile"}>
-        <img src={props.image}
-             className="card profile-image round-image"/>
-    <div className="profile-name">{props.name}</div>
+        <div className="post-container card">
+                {props.post.message}
         </div>
     )
 }
