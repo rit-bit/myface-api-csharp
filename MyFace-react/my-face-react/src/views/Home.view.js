0f0337f41c0e2542a7146ec8329a23fcaddbdc45
Charles Alexis <racharlesar@gmail.com>
2021-10-22 11:36:33 +0100

    Filtered response into our user object. And also added some cool animations

--- MyFace-react/my-face-react/src/views/Home.view.js
+++ MyFace-react/my-face-react/src/views/Home.view.js
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from 'react';
-// import users from './Test.data'
+import posts from './Test.data'
 
 export function HomeView() {
 
-    const [posts, setPosts] = useState({"items": []});
+    const [userPosts, setUserPosts] = useState();
 
     useEffect(() => {
-        try {
-            //const res = await fetch("http://localhost:5001/posts")
+            /*const res = await fetch("https://localhost:5001/posts");
+            setTest(res.json());
+            console.log(test);*/
 
             fetch("https://localhost:5001/posts")
-                .then(response => response.json())
-                .then(json => setPosts(json))
+                .then(response => {
+                    // console.log("response: " + response)
+                    return response.json()
+                })
+                .then(json => {
+                    setUserPosts(CreateUsersAndAddPosts(json))
+                })
                 .catch(error => console.log("Error: " + error))
-
-            // const json = res.json();
-            // console.log(json);
-        } catch (error) {
-            console.log("error: " + error)
-        }
+                .finally(()=>{
+            })
     }, [])
+
+
 
     return (
         <div className={"line-wrap-container"}>
-            {posts.items.map((user, index) =>
+            {posts.map((user, index) =>
                 <UserPostContainer key={"user: " + index} userdata={user} />
             )}
         </div>
@@ -58,4 +62,42 @@ function UserRecentPosts(props) {
     )
 }
 
+function CreateUsersAndAddPosts(json){
+    const users = {};
+    json.items.map((post, index)=> {
+        const id = json.items[index].postedBy.id;
+        const name = json.items[index].postedBy.displayName;
+        const profileImage = json.items[index].postedBy.profileImageUrl;
+        // console.log("Id: " + id);
+        // console.log("name: " + name);
+
+        const postObject = {
+            id: json.items[index].id,
+            message: json.items[index].message,
+            imageUrl: json.items[index].imageUrl,
+            postedAt: json.items[index].postedAt,
+        }
+
+        if (users[id]){
+            users[id].posts.push(postObject)
+        }else{
+            users[id] = {
+                name: name,
+                profileImage: profileImage,
+                posts: [
+                    postObject
+                ]
+            }
+        }
+    })
+    console.log("users length: " + users[81].posts.length);
+    console.log("user 1: " + users[2].name);
+    console.log("user 1 post: " + users[0].post[2].message);
+    return users;
+}
+
 //
+
+
+// const json = res.json();
+// console.log(json);
--- MyFace/Models/Request/SearchRequestModel.cs
+++ MyFace/Models/Request/SearchRequestModel.cs
@@ -11,6 +11,6 @@
         }
 
         public int Page { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
+        public int PageSize { get; set; } = 100;
     }
 }
